77fb4445718f6937aa405ec0dbc25da7558dbaf6
Ellen Marie Dash <me@duckie.co>
2009-10-17 12:55:41 -0400

    You can move windows, issue with resizing

--- core.c
+++ core.c
@@ -1,5 +1,36 @@
 #include <X11/Xlib.h>
 
+XButtonEvent move_start = {0};
+
+void CoreKeyPress(Display *dpy, int screenNum, Window root,
+	XEvent event)
+{
+	if(event.xkey.subwindow != None) 
+	/* Raise the window on keypress. */
+	XRaiseWindow(dpy, event.xkey.subwindow);
+}
+
+void CoreButtonPress(Display *dpy, int screenNum, Window root,
+	XEvent event)
+{
+	XWindowAttributes attr = {0};
+	if(event.xbutton.subwindow == None)
+		return;
+	
+	XRaiseWindow(dpy, event.xbutton.subwindow);
+	
+	/* Look for motion and button releases. */
+	XGrabPointer(dpy, event.xbutton.subwindow, True,
+		PointerMotionMask|ButtonReleaseMask,
+		GrabModeAsync, GrabModeAsync, None,
+		None, CurrentTime);
+	
+	/* Record start location of window movement. */
+	XGetWindowAttributes(dpy, event.xbutton.subwindow,
+		&attr);
+	move_start = event.xbutton;
+}
+
 void CoreRegister(Display *dpy, int screenNum, Window root)
 {
 	XSelectInput(dpy, root, SubstructureRedirectMask
@@ -9,15 +40,59 @@ void CoreRegister(Display *dpy, int screenNum, Window root)
 			GrabModeAsync, GrabModeAsync, None, None);
 }
 
+void CoreMotionNotify(Display *dpy, int screenNum, Window root,
+	XEvent event)
+{
+	int xdiff, ydiff;
+	XWindowAttributes attr = {0};
+	/*if (event.xmotion.subwindow != None)
+		XRaiseWindow(dpy, event.xmotion.subwindow);*/
+		
+	/* Because we received MotionNotify we are
+	 * already in pointer grab mode. */
+ 
+	/* Loop throught events as to only look at
+	 * the most recent event. */
+	while(XCheckTypedEvent(dpy, MotionNotify, &event))
+	;
+ 
+ 	xdiff = event.xbutton.x_root - move_start.x_root;
+	ydiff = event.xbutton.y_root - move_start.y_root;
+ 
+	if (move_start.button == 1) {
+		XMoveWindow(dpy, event.xmotion.window,
+		attr.x+xdiff, attr.y+ydiff);
+	} else if (move_start.button == 3) {
+
+		/* Only resize the window if it will be
+		* greater than 1 by 1. */
+		if (attr.width + xdiff >= 1 &&
+			attr.height + ydiff >= 1)
+			XResizeWindow(dpy,
+				event.xmotion.window,
+				attr.width+xdiff,
+				attr.height+ydiff);
+		else
+			XResizeWindow(dpy,
+				event.xmotion.window,
+				1, 1); 
+	}
+}
+
 void CoreEventProcess(Display *dpy, int screenNum, Window root,
 	XEvent event)
 {
 	XWindowChanges values;
 
 	switch (event.type) {
+	case KeyPress:
+		CoreKeyPress(dpy, screenNum, root, event);
+		break;
+	case ButtonPress:
+		CoreButtonPress(dpy, screenNum, root, event);
+		break;
 	case MotionNotify:
-		if (event.xmotion.subwindow != None)
-			XRaiseWindow(dpy, event.xmotion.subwindow);
+		CoreMotionNotify(dpy, screenNum, root, event);
 		break;
 	case CirculateRequest:
 		XCirculateSubwindows(event.xcirculate.display,
